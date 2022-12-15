eefafca2490cdd6d0009ff07d48948522710ab51
Yeon <30740147+zcoder83@users.noreply.github.com>
2022-12-15 15:42:51 -0500

    Add files via upload

---
+++ routes/ToDoRoute.js
@@ -0,0 +1,17 @@
+const { Router } = require("express");
+const {
+    getToDo,
+    saveToDo,
+    updateToDo,
+    deleteToDo,
+} = require("../controllers/ToDoController");
+const router = Router();
+
+// display all todo list by / route
+router.get("/", getToDo);
+
+router.post("/save", saveToDo);
+router.post("/update", updateToDo);
+router.post("/delete", deleteToDo);
+
+module.exports = router;
